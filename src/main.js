0bfc40500eb21dbb2c9b23e2f57e1ac9088d027f
leichangchun <l495735915@163.com>
2017-11-21 14:27:38 +0800

    测试插件使用

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import areaSelect from './index.js'
+// import areaSelect from './index.js' //使用本地的
+import areaSelect from 'vue-area-select-lei' //插件的方式引入
 
 Vue.use(areaSelect)
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  // entry: './src/main.js',
-  entry: './src/index.js', //打包 插件入口
+  entry: './src/main.js',
+  // entry: './src/index.js', //打包 插件入口
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    // filename: 'build.js'
-    filename: 'vue-area-select-lei.js',
-    library:'AreaSelect',
-    libraryTarget:'umd',
-    umdNamedDefine:true
+    filename: 'build.js'
+    // filename: 'vue-area-select-lei.js',
+    // library:'AreaSelect',   reqire的名字
+    // libraryTarget:'umd',
+    // umdNamedDefine:true
   },
   module: {
     rules: [
